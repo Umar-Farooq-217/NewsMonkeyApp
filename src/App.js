ff9977163f4cc8989e18e437fead097258b5af5d
Umar-Farooq-217 <uf29664@gmail.com>
2026-09-02 18:03:18 +0500

    error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
-import './App.css';
-import Navbar from './components/Navbar';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+      </div>
+    )
+  }
 }
-
 export default App;
